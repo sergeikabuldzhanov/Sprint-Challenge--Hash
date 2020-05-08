021d44a759e129cb06ffc59d91060c2024f981ac
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-05-08 13:07:52 +0500

    finder

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,10 +1,15 @@
 def finder(files, queries):
-
     """
     YOUR CODE HERE
     """
-
-    return result
+    # dictinoary with an entry for each file
+    queries_dict = {query: [] for query in queries}
+    # if last part of query is the same as filename add it to relevent entry in dict
+    for file in files:
+        query = file.split('/')[-1]
+        if query in queries_dict:
+            queries_dict[query].append(file)
+    return [file for query in queries_dict for file in queries_dict[query]]
 
 
 if __name__ == "__main__":
